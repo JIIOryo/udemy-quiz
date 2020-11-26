dc8ed7555d598ba7b66e9dbe2c21c73f9f26f670
JIIOryo <r-kawaguchi-m4w@eagle.sophia.ac.jp>
2020-11-26 21:46:39 +0900

    remove: mc-quiz-answer--correctness--3pFQG element

--- udemy_quiz.js
+++ udemy_quiz.js
@@ -3,8 +3,8 @@ javascript: (function() {
   // 背景色
   [...document.getElementsByClassName('mc-quiz-answer--correct--is6Db')].forEach(el => el.style['background-color'] = '#FFF0');
 
-  // 文字
-  [...document.getElementsByClassName('mc-quiz-answer--correctness--3pFQG')].forEach(el => el.style.color = '#FFF0');
+  // 「正解」の文字を削除する
+  [...document.getElementsByClassName("mc-quiz-answer--correctness--3pFQG")].forEach(el => el.parentNode.removeChild(el));
 
   // ラジオボタンとかがチェックできるようになる
   document.getElementsByName('answer').forEach(el => el.disabled = false);
